c7c7db1ee3349b49dcc0d94a3cc4c6aa2dbfe34a
boutaj <boutajmouad@proton.me>
2025-10-28 17:12:43 +0100

    parsing a stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -6,8 +6,15 @@ import (
 	"strings"
 )
 
+type parserState string
+const (
+	parserInit parserState = "initialized"
+	parserDone parserState = "done"
+)
+
 type Request struct {
 	RequestLine RequestLine
+	state parserState
 }
 
 type RequestLine struct {
@@ -16,38 +23,70 @@ type RequestLine struct {
 	Method        string // GET
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+outer:
+	for {
+		switch r.state {
+			case parserInit:
+				rl, n, err := parseRequestLine(data[read:])
+				if err != nil {
+					return 0, err
+				}
+				if n == 0 {
+					break outer
+				}
+				r.RequestLine = *rl
+				read += n
+
+				r.state = parserDone
+			case parserDone:
+				break outer
+		}
 	}
-	parsed, _, err := parseRequestLine(data)
-	if err != nil {
-		return nil, err
-	}
-	var rq Request
-	rq.RequestLine = *parsed
-	return &rq, nil
+	return read, nil
 }
 
-func parseRequestLine(line []byte) (*RequestLine, string, error) {
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := &Request{state: parserInit}
+
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for request.state != parserDone {
+		n, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			return nil, err
+		}
+		bufLen += n
+		readN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+		copy(buf, buf[readN:bufLen])
+		bufLen -= readN
+	}
+
+	return request, nil	
+}
+
+func parseRequestLine(line []byte) (*RequestLine, int, error) {
 	sLine := string(line)
 	index := strings.Index(sLine, "\r\n")
 
 	if index == -1 {
-		return nil, "", errors.New("failed to read request line")
+		return nil, 0, nil
 	}
 
 	rqline := sLine[:index]
-	rest := sLine[index+2:]
+	rest := index+2
 
 	parts := strings.Split(string(rqline), " ")
 	if len(parts) != 3 {
-		return nil, rest, errors.New("invalid request line format")
+		return nil, 0, errors.New("invalid request line format")
 	}
 	method, target, version := parts[0], parts[1], parts[2]
 	if !strings.HasPrefix(version, "HTTP/") {
-		return nil, rest, errors.New("Invalid HTTP version : " + version)
+		return nil, 0, errors.New("Invalid HTTP version : " + version)
 	}
 
 	return &RequestLine{
